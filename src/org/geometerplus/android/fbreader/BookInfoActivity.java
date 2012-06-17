1c1ac0cfd6e565b982e68105425d8d6ed793915f
langresser <wangjiatc@gmail.com>
2012-06-18 02:06:18 +0800

    资源修改

--- src/org/geometerplus/android/fbreader/BookInfoActivity.java
+++ src/org/geometerplus/android/fbreader/BookInfoActivity.java
@@ -19,36 +19,19 @@
 
 package org.geometerplus.android.fbreader;
 
-import java.text.DateFormat;
-import java.util.*;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
 import android.widget.*;
 
-import org.geometerplus.zlibrary.filesystem.ZLResource;
-import org.geometerplus.zlibrary.image.ZLImage;
-import org.geometerplus.zlibrary.image.ZLImageData;
-import org.geometerplus.zlibrary.image.ZLImageManager;
-import org.geometerplus.zlibrary.image.ZLLoadableImage;
-import org.geometerplus.zlibrary.util.ZLLanguageUtil;
-
 import org.socool.socoolreader.reader.R;
 
 import org.geometerplus.fbreader.library.*;
-import org.geometerplus.fbreader.network.HtmlUtil;
 
 public class BookInfoActivity extends Activity {
 	public static final String CURRENT_BOOK_PATH_KEY = "CurrentBookPath";
-
-	private final ZLResource myResource = ZLResource.resource("bookInfo");
 	private String m_currentBookPath;
 
 	@Override
@@ -72,14 +55,6 @@ public class BookInfoActivity extends Activity {
 		super.onStart();
 
 		final Book book = Book.getByPath(m_currentBookPath);
-
-		if (book != null) {
-			setupCover(book);
-			setupBookInfo(book);
-			setupAnnotation(book);
-			setupFileInfo(book);
-		}
-		
 		final Button btnOpen = (Button)findViewById(R.id.book_info_button_open);
 		final Button btnBook = (Button)findViewById(R.id.book_info_button_book);
 		final Button btnApp = (Button)findViewById(R.id.book_info_button_app);
@@ -108,95 +83,5 @@ public class BookInfoActivity extends Activity {
 		btnOpen.setOnClickListener(listener);
 		btnBook.setOnClickListener(listener);
 		btnApp.setOnClickListener(listener);
-
-		final View root = findViewById(R.id.book_info_root);
-		root.invalidate();
-		root.requestLayout();
-	}
-
-	private void setupInfoPair(int id, String key, CharSequence value) {
-		final LinearLayout layout = (LinearLayout)findViewById(id);
-		if (value == null || value.length() == 0) {
-			layout.setVisibility(View.GONE);
-			return;
-		}
-		layout.setVisibility(View.VISIBLE);
-		((TextView)layout.findViewById(R.id.book_info_key)).setText(myResource.getResource(key).getValue(0));
-		((TextView)layout.findViewById(R.id.book_info_value)).setText(value);
-	}
-
-	private void setupCover(Book book) {
-		if (book == null) {
-			return;
-		}
-
-		final ImageView coverView = (ImageView)findViewById(R.id.book_cover);
-
-		final DisplayMetrics metrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
-		final int maxHeight = metrics.heightPixels * 2 / 3;
-		final int maxWidth = maxHeight * 2 / 3;
-
-		coverView.setVisibility(View.GONE);
-		coverView.setImageDrawable(null);
-
-		final ZLImage image = book.getCover();
-
-		if (image == null) {
-			return;
-		}
-
-		if (image instanceof ZLLoadableImage) {
-			final ZLLoadableImage loadableImage = (ZLLoadableImage)image;
-			if (!loadableImage.isSynchronized()) {
-				loadableImage.synchronize();
-			}
-		}
-		final ZLImageData data = ZLImageManager.Instance().getImageData(image);
-		if (data == null) {
-			return;
-		}
-
-		final Bitmap coverBitmap = data.getBitmap(2 * maxWidth, 2 * maxHeight);
-		if (coverBitmap == null) {
-			return;
-		}
-
-		coverView.setVisibility(View.VISIBLE);
-		coverView.getLayoutParams().width = maxWidth;
-		coverView.getLayoutParams().height = maxHeight;
-		coverView.setImageBitmap(coverBitmap);
-	}
-
-	private void setupBookInfo(Book book) {
-		((TextView)findViewById(R.id.book_info_title)).setText(myResource.getResource("bookInfo").getValue());
-
-		setupInfoPair(R.id.book_title, "title", book.myTitle);
-		setupInfoPair(R.id.book_authors, "authors", book.authors());
-	}
-
-	private void setupAnnotation(Book book) {
-		final TextView titleView = (TextView)findViewById(R.id.book_info_annotation_title);
-		final TextView bodyView = (TextView)findViewById(R.id.book_info_annotation_body);
-		final String annotation = book.getPlugin().readAnnotation(book);	
-		if (annotation == null) {
-			titleView.setVisibility(View.GONE);
-			bodyView.setVisibility(View.GONE);
-		} else {
-			titleView.setText(myResource.getResource("annotation").getValue());
-			bodyView.setText(HtmlUtil.getHtmlText(annotation));
-			bodyView.setMovementMethod(new LinkMovementMethod());
-			bodyView.setTextColor(ColorStateList.valueOf(bodyView.getTextColors().getDefaultColor()));
-		}
-	}
-
-	private void setupFileInfo(Book book) {
-		((TextView)findViewById(R.id.file_info_title)).setText(myResource.getResource("fileInfo").getValue());
-
-		setupInfoPair(R.id.file_name, "name", book.m_filePath);
-		setupInfoPair(R.id.file_type, "type", null);
-		setupInfoPair(R.id.file_size, "size", null);
-		setupInfoPair(R.id.file_time, "time", null);
 	}
 }
